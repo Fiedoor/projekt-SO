fc6ff1a8afeeb181a0a650a8a428aafda2c41370
Seba <Seba@yes>
2025-12-28 14:24:25 +0100

    rura

---
+++ cmake-build-debug/CMakeFiles/projektSO.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/projektSO.dir/link.d"
+  "CMakeFiles/projektSO.dir/main.c.o"
+  "CMakeFiles/projektSO.dir/main.c.o.d"
+  "projektSO"
+  "projektSO.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/projektSO.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
